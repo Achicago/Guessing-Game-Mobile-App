e93896fc7a563de2e04054ac44e83f1fa8c094a3
achicago <achigonye@gmail.com>
2023-03-09 14:27:10 +0100

    Finishing touches

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -8,6 +8,7 @@ import PrimaryButton from "../components/ui/PrimaryButton";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
 import InstructionText from "../components/ui/InstructionText";
+import GuessLogItem from "../components/game/GuessLogItem";
 
 function generateRandomBetween(min, max, exclude) {
     const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -29,7 +30,7 @@ function GameScreen({ userNumber, onGameOver }) {
 
     useEffect(() => {
         if (currentGuess === userNumber) {
-            onGameOver();
+            onGameOver(guessRounds.length);
         }
     }, [currentGuess, userNumber, onGameOver]);
 
@@ -63,6 +64,8 @@ function GameScreen({ userNumber, onGameOver }) {
 
     }
 
+    const guessRoundsListLength = guessRounds.length;
+
     return (
         <View style={styles.screen} >
             <Title>Opponent's Guess</Title>
@@ -84,13 +87,17 @@ function GameScreen({ userNumber, onGameOver }) {
                 </View>
 
             </Card>
-            <View>
+            <View style={styles.listContainer}>
                 {/* {guessRounds.map((guessRound) => {
                     <Text key={guessRound}> {guessRound} </Text>
                 })} */}
 
                 <FlatList data={guessRounds}
-                    renderItem={(itemData) => <Text> {itemData.item}</Text>}
+                    renderItem={(itemData) =>
+                        <GuessLogItem
+                            roundNumber={guessRoundsListLength - itemData.index}
+                            guess={itemData.item} />
+                    }
                     keyExtractor={(item) => item}
                 />
 
@@ -124,5 +131,9 @@ const styles = StyleSheet.create({
     },
     buttonContainer: {
         flex: 1,
+    },
+    listContainer: {
+        flex: 1,
+        padding: 16,
     },
 })
